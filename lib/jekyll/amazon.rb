a3e4e1b66856a51db8d43d136970c3240de1dba1
tokzk <t@okzk.org>
2016-04-08 17:26:02 +0900

    initial version

---
+++ lib/jekyll/amazon.rb
@@ -0,0 +1,7 @@
+require "jekyll/amazon/version"
+
+module Jekyll
+  module Amazon
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/amazon/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Amazon
+    VERSION = "0.1.0"
+  end
+end
